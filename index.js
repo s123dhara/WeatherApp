a9298c6622631ab459791af82f10095478442a65
s123dhara <146332054+s123dhara@users.noreply.github.com>
2023-10-12 13:14:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -87,6 +87,14 @@ search.addEventListener('click',()=>{
                     document.body.style.backgroundRepeat = 'no-repeat';
                     document.body.style.backgroundPosition = 'center center';
                     break; 
+                case 'Fog':
+                    image.src = 'fogl.png';
+                    document.body.style.backgroundImage = 'url("fog.jpg")';
+                    document.body.style.backgroundRepeat = 'no-repeat';
+                    document.body.style.backgroundSize = 'cover';
+                    document.body.style.backgroundRepeat = 'no-repeat';
+                    document.body.style.backgroundPosition = 'center center';
+                    break; 
 
                 default:
                     image.src = '';
